e31d9a141a4fdb1ba57dae25ba022fbf56fa09f6
runaking <anongsack_billy@hotmail.co.uk>
2016-12-13 16:14:09 +0700

    No ActionBar Finish

--- app/src/main/java/app/runaking/whereltc/AuthenticationActivity.java
+++ app/src/main/java/app/runaking/whereltc/AuthenticationActivity.java
@@ -1,7 +1,9 @@
 package app.runaking.whereltc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+    } // Main method
+    public void  clickSignUpMain(View view) {
+        startActivity(new Intent(AuthenticationActivity.this, SignUpActivity.class));
     }
-}
+}   //Main class
