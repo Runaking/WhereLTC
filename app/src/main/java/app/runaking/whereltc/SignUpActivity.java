16671782f4e96acd2917c089982dc9e7e37932ca
runaking <anongsack_billy@hotmail.co.uk>
2016-12-13 16:56:54 +0700

    Finish 13/12/2016

--- app/src/main/java/app/runaking/whereltc/SignUpActivity.java
+++ app/src/main/java/app/runaking/whereltc/SignUpActivity.java
@@ -2,6 +2,7 @@ package app.runaking.whereltc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,6 +12,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
 
 
     @Override
@@ -27,13 +29,18 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void clickSignUpSign(View view) {
 
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+            //Have Space
+            Log.d("13decV1", "Have Space");
+
+        }
     }
-
-
-
-
-
-
 
 
 } // Main Class
